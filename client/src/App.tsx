334dda08ab9a19cbcc209863e3ee0eb834fcf4b6
Justin Hoch <justinhoch22@gmail.com>
2022-07-17 23:03:26 -0500

    Fixed logout logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   if (window.location.pathname === "/") window.location.href = "/forms"
   return (
     <div className="App">
-      {!Auth.loggedIn() ? <button onClick={() => Auth.logout()}>Sign out</button> : <></>}
+      {Auth.loggedIn() ? <button onClick={() => Auth.logout()}>Sign out</button> : <></>}
       <BrowserRouter>
         <Routes>
           <Route path="/signIn" element={<SignIn />}/>
