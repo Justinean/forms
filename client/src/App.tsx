16deb21f567112ac1ed0705645011d09a909e17b
Justin Hoch <justinhoch22@gmail.com>
2022-07-17 22:57:40 -0500

    Added auto redirect to forms on home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 function App() {
+  if (window.location.pathname === "/") window.location.href = "/forms"
   return (
     <div className="App">
       <BrowserRouter>
