744117e3d71509c587639ca280ec7fc62c55666e
Justin Hoch <justinhoch22@gmail.com>
2022-07-17 23:02:53 -0500

    Added temporary logout button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,11 +10,13 @@ import EditForms from './pages/EditForms';
 import Forms from './pages/Forms';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Auth from "./utils/auth";
 
 function App() {
   if (window.location.pathname === "/") window.location.href = "/forms"
   return (
     <div className="App">
+      {window.location.pathname !== "/signIn" && window.location.pathname !== "/signUp" ? <button onClick={() => Auth.logout()}>Sign out</button> : <></>}
       <BrowserRouter>
         <Routes>
           <Route path="/signIn" element={<SignIn />}/>
